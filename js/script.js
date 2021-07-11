dd400a8419ff5419e1fb1000693fd53a55f632e5
Nairi Areg <hnairiareg@gmail.com>
2021-07-11 05:51:08 +0400

    Sona v1

--- js/script.js
+++ js/script.js
@@ -18,9 +18,36 @@ window.intlTelInput(input, ({
 
 $(document).ready(function () {
   $('.slick').slick({
-    slidesToShow: 3,
+    slidesToShow: 5,
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 2000,
   });
 });
+
+
+document.addEventListener("DOMContentLoaded", function(){
+  window.addEventListener('scroll', function() {
+      if (window.scrollY > 550) {
+        document.getElementById('navbar_top').classList.add('fixed-top');
+
+        $('nav .container-fluid').addClass('bg-white');
+        $('nav .navbar-collapse').addClass('py-3');
+        $('nav .container-fluid img').attr('src','img/logo2.png');
+        $('nav .container-fluid img').addClass('scrolled-logo');
+        // // add padding top to show content behind navbar
+        // navbar_height = document.querySelector('.navbar').offsetHeight;
+        // document.body.style.paddingTop = navbar_height + 'px';
+      } else {
+        document.getElementById('navbar_top').classList.remove('fixed-top');
+        
+        $('nav .container-fluid').removeClass('bg-white');
+        $('nav .container-fluid img').removeClass('scrolled-logo');
+        $('nav .navbar-collapse').removeClass('py-3');
+        $('nav .container-fluid img').attr('src','img/logo.png');
+        //  // remove padding top from body
+        // document.body.style.paddingTop = '0';
+      } 
+  });
+}); 
+// DOMContentLoaded  end
